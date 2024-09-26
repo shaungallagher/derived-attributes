162a08c3619bf777ca631e448fddfe33ad863c50
Shaun Gallagher <shaun+github@pressbin.com>
2024-09-25 20:27:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="derived-attributes",
-    version="0.3.0",
+    version="0.3.1",
     author="Shaun Gallagher",
     author_email="github-shaun@pressbin.com",
     description="A Python library for applying computations to a JSON object using a Subject-Verb-Object grammar.",
